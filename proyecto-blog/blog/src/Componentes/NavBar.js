9f425c673c3c89c7aec39e876470ab61f7b3ad2e
Gonzalo <gonzalodiez97@gmail.com>
2023-11-10 17:58:25 -0300

    Instalacion de react-icons

--- proyecto-blog/blog/src/Componentes/NavBar.js
+++ proyecto-blog/blog/src/Componentes/NavBar.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
+import { BiUserCircle } from "react-icons/bi";
 
 function NavBar() {
   return (
@@ -18,7 +19,7 @@ function NavBar() {
             <Nav.Link as={Link} to="/blogs/Experiencias">Experiencias</Nav.Link>
             <Nav.Link as={Link} to="/blogs/Entrevistas">Entrevistas</Nav.Link>
             <Nav.Link as={Link} to="/blogs/Proyectos">Proyectos</Nav.Link>
-            <Nav.Link as={Link} to="/login">Iniciar Sesión</Nav.Link>
+            <Nav.Link as={Link} to="/login"><BiUserCircle /></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
